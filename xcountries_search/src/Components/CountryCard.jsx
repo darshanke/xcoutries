cf3195916002edcd5e27bb39184499311162ed6a
darshan <darshanke646@gmail.com>
2024-10-06 21:40:53 +0530

    changed

--- xcountries_search/src/Components/CountryCard.jsx
+++ xcountries_search/src/Components/CountryCard.jsx
@@ -1,14 +1,58 @@
+// import { Card, Typography, Box } from "@mui/material";
+// import React from "react";
+
+// const CountryCard = ({ countryData }) => {
+//   if (!countryData || countryData.length === 0) {
+//     return null;
+//   }
+
+//   return (
+//     <Box
+//     className="countryCard" 
+//       sx={{
+//         display: "flex",
+//         flexDirection: "row",
+//         flexWrap: "wrap",
+//         gap: "16px",
+//       }}
+//     >
+//       {countryData.map((item, index) => (
+//         <Card className="countryCard"
+//           key={index}
+         
+//         >
+//           <div
+//             style={{
+//               height: "150px",
+//               width: "150px",
+//               display: "flex",
+//               justifyContent: "center",
+//               alignItems: "center",
+//             }}
+//           >
+//             <img 
+//               src={item.flags.png}
+//               alt={item.name.common}
+//               style={{ width: "100%", height: "100%", objectFit: "cover" }}
+//             />
+//           </div>
+//           <Typography variant="body2" align="center" sx={{ marginTop: '8px' }}>
+//             {item.name.common}
+//           </Typography>
+//         </Card>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// export default CountryCard;
 import { Card, Typography, Box } from "@mui/material";
 import React from "react";
 
 const CountryCard = ({ countryData }) => {
-  if (!countryData || countryData.length === 0) {
-    return null;
-  }
-
   return (
     <Box
-    className="countryCard" 
+      className="countryCard"
       sx={{
         display: "flex",
         flexDirection: "row",
@@ -24,7 +68,7 @@ const CountryCard = ({ countryData }) => {
             flexDirection: "column",
             alignItems: "center",
             width: "200px",
-            padding: '1rem',
+            padding: "1rem",
           }}
         >
           <div
@@ -42,7 +86,7 @@ const CountryCard = ({ countryData }) => {
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
           </div>
-          <Typography variant="body2" align="center" sx={{ marginTop: '8px' }}>
+          <Typography variant="body2" align="center" sx={{ marginTop: "8px" }}>
             {item.name.common}
           </Typography>
         </Card>
@@ -52,3 +96,4 @@ const CountryCard = ({ countryData }) => {
 };
 
 export default CountryCard;
+
