eeeb9c9245d0543618c5d4b1d03b6e821d15f619
darshan <darshanke646@gmail.com>
2024-10-07 10:41:19 +0530

    changes

--- xcountries_search/src/App.js
+++ xcountries_search/src/App.js
@@ -6,41 +6,43 @@ import { useEffect, useState, useRef } from "react";
 
 function App() {
   const [list, setList] = useState([]);
-  const [originalList, setOriginalList] = useState([]); 
+  const [originalList, setOriginalList] = useState([]);
   const [search, setSearch] = useState("");
 
   const fetchData = async () => {
     try {
       const res = await axios.get(`https://restcountries.com/v3.1/all`);
-      setOriginalList(res.data); 
-      setList(res.data); 
+      setOriginalList(res.data);
+      setList(res.data);
     } catch (e) {
       console.error("Error fetching data:", e.message);
-      setList([]); 
+      setList([]);
     }
   };
 
   const debounceRef = useRef(null);
-  
+
   const processFilter = (value, debounceTime) => {
     // if (debounceRef.current) {
     //   clearTimeout(debounceRef.current);
     // }
     // debounceRef.current = setTimeout(() => {
-      const filter = originalList.filter((item) =>
+    const filter = originalList
+      .filter((item) =>
         item.name.common.toLowerCase().includes(value.toLowerCase())
-      ).sort((a, b) => a.name.common.localeCompare(b.name.common));
-      setList(filter);
+      )
+      .sort((a, b) => a.name.common.localeCompare(b.name.common));
+    setList(filter);
     // }, debounceTime);
   };
 
   useEffect(() => {
-    fetchData(); 
+    fetchData();
   }, []);
 
   useEffect(() => {
-    processFilter(search, 300); 
-  }, [search,list]);
+    processFilter(search, 300);
+  }, [search, list]);
 
   return (
     <div className="App">
@@ -48,9 +50,9 @@ function App() {
         type="text"
         placeholder="Search for countries"
         style={{ width: "40%", margin: "1rem" }}
-        onChange={(e) => setSearch(e.target.value)} 
+        onChange={(e) => setSearch(e.target.value)}
       />
-      {list.length > 0 ? ( 
+      {/* {list.length > 0 ? ( 
         <Box
          className="countryCard"
           sx={{
@@ -63,10 +65,25 @@ function App() {
         >
           <CountryCard className="countryCard" countryData={list} /> 
         </Box>
-      ) : null}
+      ) : null} */}
+      {
+        <div className="country-list">
+          {list.length > 0
+            ? list.map((country, index) => (
+                <div className="countryCard" key={index}>
+                  <img
+                    style={{ width: "100%", objectFit: "cover" }}
+                    src={country.flags.png}
+                    alt={`${country.name.common} flag`}
+                  />
+                  <p>{country.name.common}</p>
+                </div>
+              ))
+            : null}
+        </div>
+      }
     </div>
   );
 }
 
 export default App;
-
